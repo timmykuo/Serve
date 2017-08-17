d9c32ae0e2296144e1a7b5d02bf4b0444f192ab5
Timmy <Timmy@Timothys-MacBook-Air.local>
2017-08-17 17:20:58 -0400

    add allergy submenu screen

--- src/components/MainTabs/Explore/allergyFilter.js
+++ src/components/MainTabs/Explore/allergyFilter.js
@@ -2,5 +2,26 @@ import React from 'react';
 import {StyleSheet, Text, ScrollView, Image, TouchableOpacity, Switch, View} from 'react-native';
 
 export default class allergyFilter extends React.Component {
-  
+  render() {
+    return(
+      <View style={styles.categoryContainer}
+        <View style={styles.allowedAllergies}>
+          <Text> Dairy Allergy </Text>
+          <Switch
+            onValueChange = {(value) => this.setState({dairyAllergy: value})}
+            value = {this.state.dairyAllergy}/>
+          <Text> Egg Allergy </Text>
+          <Switch
+            onValueChange = {(value) => this.setState({eggAllergy: value})}
+            value = {this.state.eggAllergy}/>
+        </View>
+      </View>
+    );
+  };
 }
+
+const styles = StyleSheet.create({
+  categoryContainer: {
+    flexDirection: 'row',
+  }
+})
